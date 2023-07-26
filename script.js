f7adc8a81c90e1cf35d4bf077dc5d95c87cf7f5a
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-07-26 22:52:28 +0300

    add choosing marker to start game

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
+const btnX = document.querySelector("#btn-x");
+const btnO = document.querySelector("#btn-o");
+const board = document.querySelector("#game-board");
+
 let gameBoard = {
   board: [],
 };
 let players = {};
-const fillBoard = () => {
-  let board = document.querySelector("#game-board");
-  for (let i = 0; i < 9; i++) {
-    gameBoard.board.push("x");
-    let cell = document.createElement("div");
-    cell.classList.add("col-4", "cell");
-    cell.setAttribute("id", i);
-    board.appendChild(cell);
-    cell.textContent = gameBoard.board[i];
-    console.log(gameBoard.board);
-  }
+
+btnX.addEventListener("click", () => {
+  players.player1 = "x";
+  players.player2 = "o";
+  startGame("x");
+});
+btnO.addEventListener("click", () => {
+  players.player1 = "o";
+  players.player2 = "x";
+  startGame("o");
+});
+
+const startGame = (marker) => {
+  board.classList.remove("invisible");
 };
-fillBoard();
+
+// const addMarker = () => {
+//   for (let i = 0; i < 9; i++) {
+//     if (Math.random() > 0.5) {
+//       gameBoard.board.push("x");
+//     } else {
+//       gameBoard.board.push("o");
+//     }
+
+//     let cell = document.createElement("div");
+//     cell.classList.add("col-4", "cell");
+//     cell.setAttribute("id", i);
+//     board.appendChild(cell);
+//     cell.textContent = gameBoard.board[i];
+//     console.log(gameBoard.board);
+//   }
+// };
